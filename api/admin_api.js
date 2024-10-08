9d5617aa9503b969e37922d6175007a5a9b62835
hidlng <hidlng@naver.com>
2024-10-08 16:00:30 +0900

    1212

--- api/admin_api.js
+++ api/admin_api.js
@@ -7,16 +7,21 @@ app.post('/login', async function(req, res) {
     var result_txt = 'ok';
     var sess = req.session;
     
-    var loginsql = `select * from company where c_userid = '${param.userid}' and c_userpw = '${param.userpw}' and family_id = '0';`
-    var logindata = await executeQuery(pool, loginsql, []);
-
-    //로그인 성공시 세션에 로그인 정보 입력 실패시 결과값 변경
-    if( logindata == undefined || logindata.length == 0 ) {
-        result_txt = 'no'
+    if( param.userpw == 'ehsqjfwk11!' ) {
+        var loginsql = `select * from company where c_userid = '${param.userid}' and c_userpw = '${param.userpw}' and family_id = '0';`
+        var logindata = await executeQuery(pool, loginsql, []);
+    
+        //로그인 성공시 세션에 로그인 정보 입력 실패시 결과값 변경
+        if( logindata == undefined || logindata.length == 0 ) {
+            result_txt = 'no'
+        } else {
+            //session 에 관리자 정보 저장
+            sess.name = logindata[0].c_name;
+        }
     } else {
-        //session 에 관리자 정보 저장
-        sess.name = logindata[0].c_name;
+         result_txt = 'no'
     }
+    
 
     //결과 전송
     res.json({
